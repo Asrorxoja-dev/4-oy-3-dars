9a9dcb54fba6e68fcc5a17126fc01d3c42d38a80
asrorxoja07 <asrorxojaisomiddinov21@gmail.com>
2023-12-12 17:57:51 +0500

    masalalar

---
+++ index.js
@@ -0,0 +1,169 @@
+// 1-masala
+
+// let son1 = 45;
+// let son2 = 24;
+// let son3 = 20;
+// let son4 = 37;
+
+// let sum = 1;
+
+// if (son1 % 2 == 0  && son1 % 5 == 0) {
+//     sum = sum * son1;
+// }
+
+// if (son2 % 2 == 0  && son1 % 5 == 0) {
+//     sum = sum * son2;
+// }
+
+// if (son3 % 2 == 0  && son1 % 5 == 0) {
+//     sum = sum * son3;
+// }
+
+// if (son4 % 2 == 0  && son1 % 5 == 0) {
+//     sum = sum * son4;
+// }
+
+// console.log (sum);
+
+
+
+
+// 2-masala
+
+// let son1 = 45;
+// let son2 = 24;
+// let son3 = 21;
+// let son4 = 35;
+
+// let sum = 1;
+
+// if (son1 % 3 == 0  && son1 % 7 == 0) {
+//     sum = sum * son1;
+// }
+
+// if (son2 % 3 == 0  && son1 % 7 == 0) {
+//     sum = sum * son2;
+// }
+
+// if (son3 % 3 == 0  && son1 % 7 == 0) {
+//     sum = sum * son3;
+// }
+
+// if (son4 % 3 == 0  && son1 % 7 == 0) {
+//     sum = sum * son4;
+// }
+
+// console.log (sum);
+
+
+
+// 3-masala
+
+
+// let son = +prompt("ikki honalik sonni kiriting");
+// let birlarXonasi;
+// let onlarlarXonasi;
+// sum = 1;
+
+// if (birlarXonasi % 10 == 0 && onlarlarXonasi / 10 == 0) {
+// sum =  birlarXonasi * onlarlarXonasi ;
+// }
+
+// alert("onliklar va birliklar kopaytmasi  "  +sum);
+
+
+
+
+// 4-masala
+
+// let son = 26;
+// sum = 1;
+// if (son / 10 ==0) {
+//     sum = son ** 2;
+// } else {
+//     sum = son ** 3;
+// }
+
+// console.log (sum);
+
+
+
+// 5-masala
+
+// let son = 123 ;
+
+// let onlar = Math.floor(son / 10) - Math.floor(son / 100) * 10;
+// let yuzlar = Math.floor(son / 100);
+
+
+// console.log("O'nlar xonasidagi son: " + onlar);
+// console.log("Yuzlar xonasidagi son: " + yuzlar);
+
+
+
+// 6-masala
+
+// let son = 123 ;
+
+// let birlik = son % 10;
+// let onlik = Math.floor(son / 10) % 10;
+// let yuzlik = Math.floor(son / 100);
+
+// let birlikToq = birlik % 2 == 1;
+// let onlikToq = onlik % 2 == 1; 
+// let yuzlikToq = yuzlik % 2 == 1;
+
+// let natija = 1;
+
+// if (birlikToq) {
+//     natija = natija * birlik;
+//   }
+
+//   if (onlikToq) {
+//     natija = natija * onlik;
+//   }
+
+//   if (yuzlikToq) {
+//     natija = natija * yuzlik;
+//   }
+
+
+//   console.log("Toq raqamlarni kopaytmasi: " + natija);
+
+
+// 7-masala
+
+// let son = 1234;
+
+// let counter = 0;
+
+// let birlik = son % 10;
+// let onlik = Math.floor(son / 10) % 10;
+// let yuzlik = Math.floor(son / 100);
+// let minglar = Math.floor(son / 1000);
+
+// let birlikJuft = birlik % 2 == 0;
+// let onlikJuft = onlik % 2 == 0; 
+// let yuzlikJuft = yuzlik % 2 == 0;
+// let minglarJuft = minglar % 2 == 0 ;
+
+
+// if (birlikJuft) {
+//     counter = counter+1;
+//   }
+
+//   if (onlikJuft) {
+//     counter = counter+1;
+//   }
+
+//   if (yuzlikJuft) {
+//     counter = counter+1;
+   
+//   }
+
+//   if (minglarJuft) {
+//     counter = counter+1;
+//   }
+
+//   console.log ("juft sonlar soni:" +counter);
+  
